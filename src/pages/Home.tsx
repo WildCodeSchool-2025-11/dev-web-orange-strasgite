537848df8ef78b8ca09a97a675127a5756ad4ea9
Karim13WildCodeSchool <kde13013@gmail.com>
2026-01-27 17:30:29 +0100

    Creation page d'acceuil (photo + logo + titre)

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,13 +1,28 @@
 import Footer from "../components/Footer";
 import Header from "../components/Header";
 import "../styles/global.css";
+import "../styles/Home.css";
 
 export default function Home() {
 	return (
 		<>
 			<Header />
 			<main>
-				<h1>Bienvenue</h1>
+				<section className="hero">
+					<div className="hero-overlay">
+						<div className="hero-branding">
+							<img
+								src="/images/logo-strasgite.png"
+								alt="Logo Strasgite"
+								className="hero-logo"
+							/>
+							<h1 className="hero-title">Strasgite</h1>
+						</div>
+						<p className="hero-subtitle">
+							Maison d'hôtes à Strasbourg <span className="heart">♥</span>
+						</p>
+					</div>
+				</section>
 			</main>
 			<Footer />
 		</>
