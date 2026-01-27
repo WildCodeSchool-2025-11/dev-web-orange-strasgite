f6727afb73eb0fdfde80c4a0a4be163a6d17f312
sophiemorales6440 <sophiemorales6440@github.com>
2026-01-27 11:56:27 +0100

    Ajout coordonnées et base page contact

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -8,6 +8,33 @@ export default function Contact() {
 			<Header />
 			<main>
 				<h1>Nous contacter</h1>
+				{/*Coordonnées*/}
+				<section className="coordonnees">
+					<h2>Coordonnées</h2>
+				<p>📍Adresse : quai Mullenheim, 67000 Strasbourg</p>
+				<p>📞Téléphone : 03 88 12 34 56</p>
+				<p>✉️Email : contact@strasgite.fr</p>
+				<p>🕒Horaires d'ouverture : 9h - 18h du lundi au samedi</p>
+				</section>
+				
+				{/*Carte*/}
+				<section className="carte">  
+				<h2>Où nous trouver</h2>
+				<div className="map-container">
+					<p>Carte interactive à intégrer ici</p>
+				</div>
+				</section>
+
+				{/*Formulaire de contact*/}
+				<section className="formulaire-contact">
+					<h2>Envoyez-nous un message</h2>
+					<p>formulaire à ajouter</p>
+				</section>
+				{/*Avis clients*/}
+				<section className="avis-clients">
+					<h2>Avis de nos clients</h2>
+					<p>avis à intégrer ici</p>
+				</section>
 			</main>
 			<Footer />
 		</>
